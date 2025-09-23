82db92db08dcbad27782d9ed9491b89663594999
MayanthaS <sapumalmayantha@gmail.com>
2025-09-23 13:05:02 +0530

    Changed values JS

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //Examine the document object
+/*
 console.dir(document);
 
 //get properties
@@ -11,12 +12,39 @@ console.log(document.head);
 console.log(document.body);
 
 console.log(document.all);
+console.log(document.all[11]); 
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(document.body); // Will now show the <body> element
+});
+
+
+
+console.log(document.all);
 
 console.log(document.forms);
 console.log(document.links);
 console.log(document.images);
-
+*/
 //--change value
 console.log(document.title);
 document.title = "Document object Notation";
 console.log(document.title);
+
+//not best way
+document.all[11].textContent = "Shopping Item List"
+console.log(document.all[11]);
+
+//--select element
+
+console.log(document.getElementById('li-items'));
+var formTtile = document.getElementById('frmt');
+console.log(formTtile);
+
+
+//--textContent,innertext and inner html
+
+//formTtile.textContent ="Add shopping Items";
+formTtile.innerText ="Add shopping Items";
+//console.log(formTtile.textContent);
+console.log(formTtile.innerText);
